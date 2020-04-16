b06e15572e6f6de44706031195ee1a476e724ea2
tdj03001 <tyjenki@gmail.com>
2020-04-16 01:27:25 -0400

    Imported BrowserRouter and all new components. Set up class extension, implemented BrowserRouter and Switch for About, Portfolio, and Contact pages (routes)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from "./components/Header/header";
 import Footer from "./components/Footer/footer";
+import About from "./components/About/about";
+import Portfolio from "./components/Portfolio/portfolio";
+import Contact from "./components/Contact/contact";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <Switch>
+            <Route path="/" component={About} exact />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+            {/* <Route component={Error} /> */}
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Footer />
+//     </div>
+//   );
+// }
 
 export default App;
