280b9bdc86d9036ec9af4f26a9eb9e0bf3120304
tdj03001 <tyjenki@gmail.com>
2020-05-06 14:06:08 -0400

    restoring to older version of server.js where nodemailer was working locally

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const cors = require('cors');
-const path = require("path");
-let app = express();
 require("dotenv").config();
 
 const transport = {
@@ -51,15 +49,8 @@ router.post('/send', (req, res, next) => {
   })
 })
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static('client/public'));
-
-
+const app = express()
 app.use(cors())
-app.use('*', router)
+app.use(express.json())
+app.use('/', router)
 app.listen(3002)
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "./client/public/index.html"));
-// });
-
