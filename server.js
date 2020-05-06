8eddc7b0fbe01211716376d794dce71123e56bd2
tdj03001 <tyjenki@gmail.com>
2020-05-06 15:46:51 -0400

    added * route

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+path = require("path");
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 require("dotenv").config();
+
+PORT = process.env.PORT || 3001;
 
 const transport = {
   host: 'smtp.gmail.com',
@@ -50,7 +53,16 @@ router.post('/send', (req, res, next) => {
 })
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-app.listen(3002)
+// app.listen(3002)
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
+
+app.listen(PORT, function () {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
