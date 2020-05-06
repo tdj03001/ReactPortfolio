54b5ab298cc56b9c1e02a80b21343d31d0b5f337
tdj03001 <tyjenki@gmail.com>
2020-05-05 23:26:54 -0400

    added more express configuration

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const app = express();
 require("dotenv").config();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static("public"));
 
 const transport = {
@@ -51,8 +54,7 @@ router.post('/send', (req, res, next) => {
   })
 })
 
-const app = express()
+
 app.use(cors())
-app.use(express.json())
 app.use('/', router)
 app.listen(3002)
